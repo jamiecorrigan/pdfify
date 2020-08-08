89021515fe4368776b769c208e774adf6991d292
Jamie Corrigan <38882200+jamiecorrigan@users.noreply.github.com>
2020-08-09 02:28:54 +1000

    feat: add ImageInput component.

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,46 @@
   <VApp>
     <VMain>
       <VContainer>
-        In Development
+        <VRow>
+          <VCol cols="12">
+            <ImageInput @add="addImages" />
+          </VCol>
+        </VRow>
+        <VRow>
+          <VCol cols="12">
+            {{ images.map((f) => f.name) }}
+          </VCol>
+        </VRow>
+        <VRow>
+          <VCol cols="12">
+            <VBtn @click="clearImages">
+              Clear All
+            </VBtn>
+          </VCol>
+        </VRow>
       </VContainer>
     </VMain>
   </VApp>
 </template>
 
 <script>
+import ImageInput from '@/components/ImageInput';
+
 export default {
   name: 'App',
-
-  data: () => ({}),
+  components: {
+    ImageInput,
+  },
+  data: () => ({
+    images: []
+  }),
+  methods: {
+    addImages(newImages) {
+      this.images = this.images.concat(newImages);
+    },
+    clearImages() {
+      this.images = [];
+    },
+  },
 };
 </script>
---
+++ src/components/ImageInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="d-flex align-center">
+    <VFileInput
+      v-model="files"
+      label="Images"
+      accept="image/*"
+      multiple
+      hide-details
+      dense
+    />
+    <VBtn
+      :disabled="!files.length"
+      color="success"
+      class="ml-4"
+      fab
+      x-small
+      @click="commit"
+    >
+      <VIcon>add</VIcon>
+    </VBtn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ImageInput',
+  data: () => ({
+    files: []
+  }),
+  methods: {
+    commit() {
+      this.$emit('add', this.files);
+      this.files = [];
+    },
+  },
+};
+</script>
